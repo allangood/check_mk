85ab45d24ac7d94c5cf31489c29633956dbd6995
Allan GooD <allan.cassaro@gmil.com>
2016-04-22 08:09:48 -0300

    Added scan_function on all snmp checks

---
+++ plugins/repack.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# by Allan GooD
+#
+
+dirs="agents checkman checks doc inventory notifications pnp-templates web"
+
+basedir="$(pwd)"
+for info in $(find -name info); do
+  pktdir="$(dirname ${info})"
+  pktname="$(grep \'name\' ${info} | cut -f4 -d\')"
+  pktversion="$(grep \'version\' ${info} | cut -f4 -d\')"
+  echo "${pktname} - ${pktversion}"
+  cd "${pktdir}"
+  for dir in ${dirs}; do
+    if [ -d "${dir}" ]; then
+      cd "${dir}"
+      tar cf ../${dir}.tar *
+      cd ..
+    fi
+  done
+  tar zcf "${pktname}-${pktversion}.mkp" info *.tar
+  rm *.tar
+  cd ${basedir}
+done
+
+exit 0
