6e75ee7101f2e6f4030525c66b9240fd9574d1fb
Paul Schaefer <schaefer.pp@gmail.com>
2021-04-08 13:52:06 +0200

    squid: Fix Python syntax of WATO-plugin

--- plugins/squid/web/plugins/wato/check_squid.py
+++ plugins/squid/web/plugins/wato/check_squid.py
@@ -8,39 +8,35 @@ Transform(
                    title = _("Set levels for Client Requests"),
                    elements = [
                          Integer(title = _("Warning at"), default_value = 600),
-                         Integer(title = _("Critical at"), default_value = 800)],
+                         Integer(title = _("Critical at"), default_value = 800)])
             ),
-        ],
             ( "client_hits",
                Tuple(
                    title = _("Set levels for Client Hits"),
                    elements = [
                          Integer(title = _("Warning at"), default_value = 600),
-                         Integer(title = _("Critical at"), default_value = 800)],
+                         Integer(title = _("Critical at"), default_value = 800)])
             ),
-        ],
             ( "server_reqps",
                Tuple(
                    title = _("Set levels for Server Requests"),
                    elements = [
                          Integer(title = _("Warning at"), default_value = 600),
-                         Integer(title = _("Critical at"), default_value = 800)],
+                         Integer(title = _("Critical at"), default_value = 800)])
             ),
-        ],
             ( "dns_time",
                Tuple(
                    title = _("Set levels for DNS response time in seconds"),
                    elements = [
                          Integer(title = _("Warning at"), default_value = 2),
-                         Integer(title = _("Critical at"), default_value = 4)],
+                         Integer(title = _("Critical at"), default_value = 4)])
             ),
-        ],
             ( "cpu_time",
                Tuple(
                    title = _("Set levels for Squid CPU time in percent"),
                    elements = [
                          Integer(title = _("Warning at"), default_value = 60),
-                         Integer(title = _("Critical at"), default_value = 80)],
+                         Integer(title = _("Critical at"), default_value = 80)])
             ),
         ]
     ),
